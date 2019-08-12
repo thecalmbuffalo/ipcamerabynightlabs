8954f59a243d8d6ce914b2e2a2c5465241e8a4e5
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-12 17:48:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 var constraints = { video: { facingMode: "environment" }, audio: false };
-
+var track = null;
 const cameraView = document.querySelector("#camera--view"),
       cameraOutput = document.querySelector("#camera--output"),
       cameraSensor = document.querySelector("#camera--sensor"),
-      camseraTrigger = document.querySelector("#camera--trigger")
-
+      camseraTrigger = document.querySelector("#camera--trigger");
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+const select = document.getElementById('select');
+function gotDevices(mediaDevices) {
+  select.innerHTML = '';
+  select.appendChild(document.createElement('option'));
+  let count = 1;
+  mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  });
+}
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
-        track = stream.getTracks() [0];
-        cameraView.srcObject = stream;
-    })
+            track = stream.getTracks() [0];
+            cameraView.srcObject = stream;
     .catch(function(error) {
-        console.errot("Oops. Something is broken.", error);
+        console.error("Oops.", error);
     });
 }
-
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
@@ -24,6 +39,4 @@ cameraTrigger.onclick = function() {
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
-
-window.addEventListener("load", cameraStart, true);
-
+window.addEventListener("load", cameraStart, false);
